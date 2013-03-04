35ee1a1fb24c3f482343485440594e6a9877e58e
fquesada <fabian.quesada.s@gmail.com>
2013-03-04 15:44:12 -0600

    Usuario Provisional y Autenticacion

---
+++ protected/views/site/crearusuarioprovisional.php
@@ -0,0 +1,5 @@
+<form name="input" method="POST">
+        Username: <input type="text" name="user">
+        Password: <input type="text" name="password">
+        <input type="submit" value="Crear Usuario">
+</form>
