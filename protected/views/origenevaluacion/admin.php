742c3bbf6eee70c5062fe4e62bc7f66040c7782d
William <willcha9019@gmail.com>
2013-03-15 11:48:30 -0600

    Correccion de Crud - Origenevaluacion

--- protected/views/origenevaluacion/admin.php
+++ protected/views/origenevaluacion/admin.php
@@ -31,13 +31,6 @@ Puede ingresar opcionalmente un operador comparativo (<b>&lt;</b>, <b>&lt;=</b>,
  <b>=</b>) al inicio de cada valor de búsqueda para especificar cómo se debe realizar la comparación.
 </p>
 
-<?php echo CHtml::link('Busqueda Avanzada','#',array('class'=>'search-button')); ?>
-<div class="search-form" style="display:none">
-<?php $this->renderPartial('_search',array(
-	'model'=>$model,
-)); ?>
-</div><!-- search-form -->
-
 <?php $this->widget('zii.widgets.grid.CGridView', array(
 	'id'=>'origenevaluacion-grid',
 	'dataProvider'=>$model->search(),
