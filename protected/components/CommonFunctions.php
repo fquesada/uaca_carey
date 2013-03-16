e0a1f26b04aa062a925253c4b96a1993ebb1cdf0
fquesada <fabian.quesada.s@gmail.com>
2013-03-15 19:53:18 -0600

    Common Functions

--- protected/components/CommonFunctions.php
+++ protected/components/CommonFunctions.php
@@ -6,11 +6,40 @@
  */
 class CommonFunctions {
 	
-    public function DateMysqltoPhp(){
-        
+    /*
+     * @param $datetime fecha Mysql con el formato Y-m-d H:i:s'
+     * @return string fecha con el formato D-M-Y
+     */
+    public function datemysqltophp($datetime){         
+        list($y, $m, $d) = explode('-', substr($datetime, 0, 10));              
+        return $d.'-'.$m.'-'.$y;    
     }
     
-    public function DatePhptoMysql(){
-        
+    /*
+     * @param $date string con el formato d-m-Y
+     * @return date fecha con el formato Y-m-d
+     */
+    public function datephptomysql($date){
+        list($d, $m, $y) = explode('-', $date);         
+        return strftime('%Y-%m-%d', mktime(0, 0, 0, $m, $d, $y));
+    }
+    
+    /*
+     * @return date fecha con el formato Y-m-d para almacenar en MYSQL
+     */
+    public function datenow(){
+        return datephptomysql(date('d-m-Y'));
+    }
+    
+    /*
+     * @param $stringnumber el cual debe comprobarse con anticipacion si es un numero,
+     * para estopuede utilizar la funcion is_numeric
+     * @return int o float segun sea el caso
+     */
+    public function stringtonumber($stringnumber) {//Para utilizar esta funcion se debe comprobar con anticipacion que el var es numerico
+    if ((float) $stringnumber != (int) $stringnumber)
+        return (float) $stringnumber;
+    else
+        return (int) $stringnumber;
     }
 }
