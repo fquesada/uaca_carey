351efd2daccf59aad2252d457446b825f6235a32
fedeguz <fedeguz1234@hotmail.com>
2013-03-25 16:05:38 -0600

    Pagina de inicio

--- protected/config/main.php
+++ protected/config/main.php
@@ -7,7 +7,7 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'UACA',
+	'name'=>'Gestión por Competencias',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
