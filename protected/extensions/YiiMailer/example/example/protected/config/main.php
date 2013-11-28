e475f5b8f1fa43710c04815e2bbca24f2b34eb8f
William <willcha9019@gmail.com>
2013-11-28 15:24:29 -0600

    Pruebas de mail

--- protected/extensions/YiiMailer/example/example/protected/config/main.php
+++ protected/extensions/YiiMailer/example/example/protected/config/main.php
@@ -7,54 +7,38 @@
 // CWebApplication properties can be configured here.
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
-	'name'=>'Gestión por Competencias',
+	'name'=>'My Web Application',
 
 	// preloading 'log' component
 	'preload'=>array('log'),
-        'sourceLanguage' => 'en',
-        'language' => 'es',
 
 	// autoloading model and component classes
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
-                'application.components.jpgraph.*',
-                 'ext.YiiMailer.YiiMailer',
+		'ext.YiiMailer.YiiMailer',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		
+		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'uaca',
-			// If removed, Gii defaults to localhost only. Edit carefully to taste.
+			'password'=>'Enter Your Password Here',
+		 	// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
 		),
-            
-//                'rights'=>array( 
-//                    'superuserName'=>'admin',                    
-//                    'authenticatedName'=>'Authenticated',
-//                    'userClass' => 'Usuario',
-//                    'userIdColumn'=>'id',
-//                    'userNameColumn'=>'login',                    
-//                    
-//                    'install'=>false),
-
+		*/
 	),
-        
-        'homeUrl'=>array('site/login'),
+
 	// application components
 	'components'=>array(
 		'user'=>array(
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
-                        'loginUrl'=>array('site/login'),
-                        //'class'=>'RWebUser',
 		),
-                //'authManager'=>array( 'class'=>'RDbAuthManager'),
 		// uncomment the following to enable URLs in path-format
-		
+		/*
 		'urlManager'=>array(
 			'urlFormat'=>'path',
 			'rules'=>array(
@@ -63,24 +47,24 @@ return array(
 				'<controller:\w+>/<action:\w+>'=>'<controller>/<action>',
 			),
 		),
-		
-//		'db'=>array(
-//			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-//		),
-		// uncomment the following to use a MySQL database
-		
+		*/
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=uacasec',
+			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+		),
+		// uncomment the following to use a MySQL database
+		/*
+		'db'=>array(
+			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
-			'username' => 'uaca_admin',
-			'password' => 'dbUACAsecadmin13!',
+			'username' => 'root',
+			'password' => '',
 			'charset' => 'utf8',
 		),
-		
+		*/
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
-			'errorAction'=>'site/error',
-		),
+            'errorAction'=>'site/error',
+        ),
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
@@ -88,11 +72,6 @@ return array(
 					'class'=>'CFileLogRoute',
 					'levels'=>'error, warning',
 				),
-                                array(
-                                    'class'=>'CFileLogRoute',
-                                    'levels'=>'info, trace',
-                                    'logFile'=>'info.log',
-                                ),
 				// uncomment the following to show log messages on web pages
 				/*
 				array(
@@ -109,5 +88,4 @@ return array(
 		// this is used in contact page
 		'adminEmail'=>'webmaster@example.com',
 	),
-        
 );
